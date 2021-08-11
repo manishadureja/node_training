6cbeff14979f0aa8b02739b669644c6c4552d801
Manisha Dureja <myselfmanisha@gmail.com>
2021-08-11 18:34:49 +1000

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLOBALS - NO WINDOW !!!!
+
+//_dirname - path to current directory
+//_filename - file name
+// require - function to use modules (COMMONJS)
+// module  - info about current module (file)
+// process - info about env where the program is being executed
+
+console.log(_dirname);
+setInterval(() => {
+  console.log("hello world");
+}, 1000);
